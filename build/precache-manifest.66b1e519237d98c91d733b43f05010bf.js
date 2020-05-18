ce46c8821ea58885396fb4074857b9adec7bf39c
Tim Jones <timjonez@protonmail.com>
2020-05-18 15:49:04 -0400

    production build

---
+++ build/precache-manifest.66b1e519237d98c91d733b43f05010bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accf26cc8be5f826d39d6fcd8b146b05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cddd64279536ca25981",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0cddd64279536ca25981",
+    "url": "/static/js/2.9e21ffaf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9e21ffaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c220448ebd434c9678a1",
+    "url": "/static/js/main.971a162b.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
